1c4e0ddf0ca77f9a6f75774b81c184d3ba6e2d4e
Brandon <hutchingsonb@gmail.com>
2021-06-03 13:26:22 -0400

    updated header component to display to page

--- employee-directory/src/components/Header.js
+++ employee-directory/src/components/Header.js
@@ -1,1 +1,12 @@
 import React from "react";
+
+function Header() {
+    return (
+      <div className="p-5 text-center bg-dark">
+        <h1 className="mb-3 text-light">Employee Directory</h1>
+        <h4 className="mb-3 text-light">Click on carrots to filter by heading or use the search box to narrow your results</h4>
+      </div>
+    );
+}
+
+export default Header;
