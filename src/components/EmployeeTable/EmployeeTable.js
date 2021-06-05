e0bab98118b6e32c08231f7b53fc74efeb2459c1
Brandon <hutchingsonb@gmail.com>
2021-06-04 23:23:07 -0400

    created employee table with placeholder data

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -3,7 +3,40 @@ import React from "react";
 const EmployeeTable = () => {
   return (
     <div>
-      
+      <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Email</th>
+      <th scope="col">DOB</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>420-420-6969</td>
+      <td>@mdo</td>
+      <td>69-69-6969</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>420-420-6969</td>
+      <td>@fat</td>
+      <td>69-69-6969</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>420-420-6969</td>
+      <td>@twitter</td>
+      <td>69-69-6969</td>
+    </tr>
+  </tbody>
+</table>
     </div>
   )
 }
