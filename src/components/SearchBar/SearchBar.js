cde5ced8e9bcabe5f7edc85657cca4bc7238ee9a
Brandon <hutchingsonb@gmail.com>
2021-06-09 21:47:46 -0400

    updating searchbar styles

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,7 +11,7 @@ const SearchBar = (props) => {
   }
 
   return (
-    <div className="input-group">
+    <div style={{width: "80%", margin: "50px 55vw 50px 45vw" }} className="input-group">
       <div className="form-outline">
         <input type="search"  id="form1" onChange={handleChange} placeholder="Search" className="form-control" />
       </div>
